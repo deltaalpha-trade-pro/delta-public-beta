c16894ddbf10fb8f046f9613bb97c762283363e5
deltaalpha-trade-pro <222595377+deltaalpha-trade-pro@users.noreply.github.com>
2026-07-02 03:32:12 +0100

    fix: remove invalid ThemeProviderProps import from next-themes

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,22 @@
-'use client'
+"use client"
 
-import * as React from 'react'
 import {
   ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from 'next-themes'
+} from "next-themes"
+import type { ReactNode } from "react"
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+type ThemeProviderProps = {
+  children: ReactNode
+  [key: string]: any
+}
+
+export function ThemeProvider({
+  children,
+  ...props
+}: ThemeProviderProps) {
+  return (
+    <NextThemesProvider {...props}>
+      {children}
+    </NextThemesProvider>
+  )
 }
